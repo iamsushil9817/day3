2f62378ab1138c1c966321fb662245cad7be1026
sushil <parshid123@gmail.com>
2023-06-21 08:21:53 +0545

    object using function

--- c/c.js
+++ c/c.js
@@ -4,13 +4,29 @@
 
 // add(5,6)
 
-const a={
-    fname:"sushil",
-    lname:"bhandri",
-    age:21,
-    enroll : () => {
-        console.log("you are successfuly enrolled");
-    },
-};
+// const a={
+//     fname:"sushil",
+//     lname:"bhandri",
+//     age:21,
+//     enroll : () => {
+//         console.log("you are successfuly enrolled");
+//     },
+// };
 
+// a.enroll();
+// a.email="sushil123@mail.com"
+// console.log(a)
+function createa(){
+    return {
+        fname:"sushil",
+        lname:"bhandri",
+        age:21,
+        enroll : () => {
+            console.log("you are successfuly enrolled");
+        },
+    } 
+     
+}
+
+const a = createa();
 a.enroll();
