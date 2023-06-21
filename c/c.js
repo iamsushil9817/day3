9609db45813c10b995464726d797eef511850061
sushil <parshid123@gmail.com>
2023-06-21 08:01:43 +0545

    learning objects

---
+++ c/c.js
@@ -0,0 +1,16 @@
+// const add = (a,b) =>{
+//     console.log(a+b);
+// }
+
+// add(5,6)
+
+const a={
+    fname:"sushil",
+    lname:"bhandri",
+    age:21,
+    enroll : () => {
+        console.log("you are successfuly enrolled");
+    },
+};
+
+a.enroll();
