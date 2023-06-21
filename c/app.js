172f9168d4ecd9587ca4e9079e5fb5563b94b01b
sushil <parshid123@gmail.com>
2023-06-21 13:33:06 +0545

    backend

---
+++ c/app.js
@@ -0,0 +1,7 @@
+const  http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
+console.log('App running on port 8080');
